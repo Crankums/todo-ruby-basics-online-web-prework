ec33d3ad24a9df9bc29880ed13d0bacec6499642
Craig Riley Donavin <crdonavin@gmail.com>
2019-02-28 19:08:39 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -2,12 +2,12 @@ def division(num1, num2)
  num1/num2
 end
 
-def assign_variable(value)
-value
+def assign_variable(name)
+name
 end
 
-def argue
-
+def argue(argument)
+  argument
 end
 
 def greeting
